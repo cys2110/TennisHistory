10c543d81b6aa49c3d259e41ef3b319afa257688
cys2110 <csheridan2110@gmail.com>
2024-06-04 11:25:14 -0400

    typescript setup

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <RouterView :key="$route.fullPath" />
+    </v-main>
+  </v-app>
+</template>
---
+++ client/src/main.ts
@@ -0,0 +1,69 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { aliases, fa } from 'vuetify/iconsets/fa-svg'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fad } from '@fortawesome/pro-duotone-svg-icons'
+import { fas } from '@fortawesome/pro-solid-svg-icons'
+import { far } from '@fortawesome/pro-regular-svg-icons'
+import { mdi } from 'vuetify/iconsets/mdi'
+import { VCard, VImg, VSheet } from 'vuetify/lib/components/index.mjs'
+
+const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'dark'
+    },
+    date: {
+        locale: {
+            en: 'en-GB'
+        }
+    },
+    icons: {
+        defaultSet: 'fa',
+        aliases,
+        sets: {
+          fa,
+          mdi
+        }
+    },
+    aliases: {
+        ViewSheet: VSheet,
+        ShortCard: VCard,
+        FlagImg: VImg
+    },
+      defaults: {
+        ViewSheet: {
+          class: 'bg-transparent my-10 pa-3 w-75 mx-auto'
+        },
+        ShortCard: {
+          variant: 'elevated',
+          color: 'indigo-darken-4',
+          rounded: 'xl'
+        },
+        VAvatar: {
+          variant: 'outlined'
+        },
+        FlagImg: {
+          rounded: 'lg',
+          class: 'border-thin'
+        }
+    }
+})
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+library.add(fas)
+library.add(far)
+library.add(fad)
+
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
