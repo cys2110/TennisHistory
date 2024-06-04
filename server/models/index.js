bf9b3e183a8b422c84e7dc82324ddfb4e495bda9
cys2110 <csheridan2110@gmail.com>
2024-06-04 09:43:53 -0400

    backend index updated

--- server/models/index.js
+++ server/models/index.js
@@ -2,15 +2,15 @@ const dotenv = require('dotenv')
 dotenv.config()
 const config = require('../config/config')
 const Sequelize = require('sequelize')
-// const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.PASSWORD, {
-//     host: process.env.HOST,
-//     dialect: process.env.dialect
-// })
-
-const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-    host: config.HOST,
-    dialect: config.dialect
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: process.env.dialect
 })
+
+// const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+//     host: config.HOST,
+//     dialect: config.dialect
+// })
 
 const db = {}
 
