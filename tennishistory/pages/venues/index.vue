2e6414ed3b2877a881331e34a582d5c140f54bbf
cys2110 <csheridan2110@gmail.com>
2025-04-30 08:09:46 -0400

    venue pages

--- tennishistory/pages/venues/index.vue
+++ tennishistory/pages/venues/index.vue
@@ -1,7 +1,75 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({ title: "Venues", templateParams: { subPage: null } })
+const appConfig = useAppConfig()
+const toast = useToast()
+
+// Breadcrumbs
+const breadcrumbs = [
+  { label: "Home", to: { name: "home" }, icon: ICONS.home },
+  { label: "Venues", icon: ICONS.venue }
+]
+
+const selectedLetter = ref<string>("All")
+const page = ref(1)
+const pageSize = ref(25)
+
+interface VenuesAPIResponse {
+  count: number
+  cities: {
+    city: string
+    country: CountryInterface
+    venues: Pick<VenueInterface, "id" | "name">[]
+  }[]
+}
+
+// API call
+const { data, status } = await useFetch<VenuesAPIResponse>("/api/venues/all-venues", {
+  query: {
+    letter: selectedLetter,
+    skip: computed(() => (page.value - 1) * pageSize.value),
+    limit: pageSize
+  },
+  onResponseError: ({ error }) => {
+    toast.add({
+      title: "Error fetching venues",
+      description: error?.message,
+      icon: appConfig.ui.icons.error,
+      color: "error"
+    })
+    showError(error!)
+  }
+})
+</script>
 
 <template>
-  <div>
-    <nuxt-layout name="default">Page: venues/index</nuxt-layout>
-  </div>
+  <paginated-overview
+    :breadcrumbs
+    v-model="selectedLetter"
+    v-model:page="page"
+    v-model:page-size="pageSize"
+    :count="data?.count"
+  >
+    <u-page-grid
+      v-if="data?.count || ['pending', 'idle'].includes(status)"
+      class="mt-10"
+    >
+      <venue-card
+        v-if="data"
+        v-for="city in data.cities"
+        :key="city.city"
+        :city
+      />
+
+      <base-loading-card
+        v-else
+        v-for="_ in 15"
+        :key="_"
+      />
+    </u-page-grid>
+
+    <error-message
+      v-else
+      title="No venues found"
+    />
+  </paginated-overview>
 </template>
